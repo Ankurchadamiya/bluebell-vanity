a79536d6e563d9b5d2078e9ba6381507490c7b5d
ankurchadamiya <ankurpatel4008@gmail.com>
2025-04-22 09:40:49 +0530

    contact change

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const body=document.body;
-body.addEventListener("click",()=>{
-    console.log("click event occureed!!");
-})
+// const body=document.body;
+// body.addEventListener("click",()=>{
+//     console.log("click event occureed!!");
+// })
+
+function sendEmail(){
+    let param= {
+        name:document.getElementById("username").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+        Phone: document.getElementById("Phone").value
+    }
+    emailjs.send("service_aiqjlq2", "template_0f16hqv",param).then(alert("email sent !!"));
+}
